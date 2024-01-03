8ac84e33c0ae334028a090504aea685dc94f0e97
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-03 09:38:03 +0530

    api

---
+++ server/controllers/users.js
@@ -0,0 +1,17 @@
+import User from "../models/User";
+
+//READ
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ messege: err.messege });
+  }
+};
+
+
+export const getUserFriends = async(req,res)=>{
+  
+}
--- server/index.js
+++ server/index.js
@@ -39,10 +39,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES WITH FILES
-app.post("./auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
 
 // ROUTES
-app.use("auth", authRoutes);
+app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
