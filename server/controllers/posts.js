c32e001cc1e1f31486088ebf8e445450e03ce3df
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-03 16:56:58 +0530

    posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,58 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 //CREATE
 
-export const createPost = async (req, res)=>{
-  
-}
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comment: [],
+    });
+    await newPost.save();
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(409).json({ messege: err.messege });
+  }
+};
+
+//READ
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(409).json({ messege: err.messege });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(409).json({ messege: err.messege });
+  }
+};
+//UPDATE
+
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(409).json({ messege: err.messege });
+  }
+};
