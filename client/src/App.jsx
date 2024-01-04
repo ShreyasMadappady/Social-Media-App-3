74108f191026ea2b005075820a68700c551793be
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-04 23:51:46 +0530

    Removed-Materialui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,19 @@
-import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import homePage from "scenes/homePage/homePage";
-import loginPage from "scenes/loginPage/loginPage";
-import profilePage from "scenes/profilePage/profilePage";
-import { themeSettings } from "theme";
+import loginPage from "./scenes/loginPage/LoginPage";
+import homePage from "./scenes/homePage/HomePage";
+import profilePage from "./scenes/profilePage/ProfilePage";
 
 function App() {
-  const mode = useSelector((state) => {
-    state.mode;
-  });
-
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div>
+    <div className="app">
       <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Routes>
-            <Route path="/" Component={loginPage} />
-            <Route path="/home" Component={homePage} />
-            <Route path="/profile/:userId" Component={profilePage} />
-          </Routes>
-        </ThemeProvider>
+        <Routes>
+          <Route path="/" Component={loginPage} />
+          <Route path="/home" Component={homePage} />
+          <Route path="/profile/:userId" Component={profilePage} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
