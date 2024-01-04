812def7a1ea53f2d2925e9b5de075cb52e7e002f
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-04 23:24:23 +0530

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import homePage from "scenes/homePage/homePage";
 import loginPage from "scenes/loginPage/loginPage";
 import profilePage from "scenes/profilePage/profilePage";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => {
+    state.mode;
+  });
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={loginPage} />
-          <Route path="/home" Component={homePage} />
-          <Route path="/profile/:userId" Component={profilePage} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" Component={loginPage} />
+            <Route path="/home" Component={homePage} />
+            <Route path="/profile/:userId" Component={profilePage} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
