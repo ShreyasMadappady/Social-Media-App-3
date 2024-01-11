514c3b2b6af2e6568949f13596d0492310f2c62f
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-11 22:43:51 +0530

    login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import homePage from "./scenes/homePage/HomePage";
 import profilePage from "./scenes/profilePage/ProfilePage";
 import LoginPage from "./scenes/loginPage/LoginPage";
+import RegisterPage from "./scenes/registerPage/RegisterPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" Component={LoginPage} />
+          <Route path="/register" Component={RegisterPage} />
           <Route path="/home" Component={homePage} />
           <Route path="/profile/:userId" Component={profilePage} />
         </Routes>
--- client/src/scenes/registerPage/RegisterPage.jsx
+++ client/src/scenes/registerPage/RegisterPage.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function LoginPage() {
+function RegisterPage() {
   const navigate = useNavigate();
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [picturePath, setPicturePath] = useState("");
@@ -32,10 +31,54 @@ function LoginPage() {
       <h1 className="text-cyan-500 font-bold text-2xl flex justify-center bg-white py-4 ">
         Sociopedia
       </h1>
-      <form className="flex justify-center flex-col w-3/6 mx-auto bg-white p-5 my-6 rounded-xl">
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center flex-col w-3/6 mx-auto bg-white p-5 my-6 rounded-xl"
+      >
         <h1 className=" text-black font-medium">
           Welcome to Sociopedia, the Social Media for Sociopath
         </h1>
+        <input
+          className="border-2 mt-7 py-2 px-2 rounded-md"
+          type="text"
+          placeholder="FirstName"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        <input
+          className="border-2 mt-7 py-2 px-2 rounded-md"
+          type="text"
+          placeholder="LastName"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />{" "}
+        <input
+          className="border-2 mt-7 py-2 px-2 rounded-md"
+          type="file"
+          placeholder="Add picture here"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />{" "}
+        <input
+          className="border-2 mt-7 py-2 px-2 rounded-md"
+          type="text"
+          placeholder="Location"
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+        />{" "}
+        <input
+          className="border-2 mt-7 py-2 px-2 rounded-md"
+          type="text"
+          placeholder="Occupation"
+          onChange={(e) => {
+            setOccupation(e.target.value);
+          }}
+        />{" "}
         <input
           className="border-2 mt-7 py-2 px-2 rounded-md"
           type="text"
@@ -43,7 +86,7 @@ function LoginPage() {
           onChange={(e) => {
             setEmail(e.target.value);
           }}
-        />
+        />{" "}
         <input
           className="border-2 my-7 py-2 px-2 rounded-md"
           type="text"
@@ -53,19 +96,19 @@ function LoginPage() {
           }}
         />
         <button className="bg-cyan-500 text-white py-3 rounded-md">
-          Login
+          Register
         </button>
         <h2
           className="text-cyan-500 mt-7 text-sm"
           onClick={() => {
-            navigate("/register");
+            navigate("/");
           }}
         >
-          Don't have an account?, Sign Up here
+          Already have an account?, Login here.
         </h2>
       </form>
     </div>
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
