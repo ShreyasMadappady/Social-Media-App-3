b153ec97eae58acf9767c69cbda3f66e1e2f224a
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-17 19:40:38 +0530

    c

--- client/src/scenes/loginPage/LoginPage.jsx
+++ client/src/scenes/loginPage/LoginPage.jsx
@@ -36,6 +36,25 @@ function LoginPage() {
       <h1 className="text-cyan-500 font-bold text-2xl flex justify-center bg-white py-4 ">
         Sociopedia
       </h1>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256" style={{animation: "pulsateCircle 2s ease-in-out infinite"}}>
+    <style>
+      {`
+        @keyframes pulsateCircle {
+          0%, 100% {
+            transform: scale(1);
+          }
+          50% {
+            transform: scale(1.2);
+          }
+        }
+      `}
+    </style>
+    <path d="M208,32a8,8,0,0,1-8,8H56a8,8,0,0,1,0-16H200A8,8,0,0,1,208,32ZM176,72V224a16,16,0,0,1-16,16H96a16,16,0,0,1-16-16V72A16,16,0,0,1,96,56h64A16,16,0,0,1,176,72Zm-16,0H96V224h64Z"></path>
+  </svg>
+
+
+
+
       <form className="flex justify-center flex-col w-3/6 mx-auto bg-white p-5 my-6 rounded-xl">
         <h1 className=" text-black font-medium">
           Welcome to Sociopedia, the Social Media for Sociopath
